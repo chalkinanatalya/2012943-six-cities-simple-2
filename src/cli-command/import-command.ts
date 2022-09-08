fcd997c736181736608102aed1733f2e75dfbae0
Natalya <m_d.r.lecter@protonmail.com>
2022-09-09 02:27:51 +0400

    added chalk

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import TSVFileReader from '../common/file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 
@@ -8,14 +9,16 @@ export default class ImportCommand implements CliCommandInterface {
 
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
+      const output = fileReader.toArray();
+      output[0].title = chalk.red(output[0].title);
+      console.log(output);
     } catch (err) {
 
       if (!(err instanceof Error)) {
         throw err;
       }
 
-      console.log(`Не удалось импортировать данные из файла по причине: «${err.message}»`);
+      console.log(chalk.red('Не удалось импортировать данные из файла по причине: ', chalk.bold(`«${err.message}»`)));
     }
   }
 }
