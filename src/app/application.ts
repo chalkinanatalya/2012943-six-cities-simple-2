63d822c33f2144c4acdc14f116e912c4ad235863
Natalya <m_d.r.lecter@protonmail.com>
2022-09-18 18:19:12 +0400

    config and convict

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,13 +1,18 @@
+import { ConfigInterface } from '../common/config/config.interface.js';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
 
 export default class Application {
   private logger!: LoggerInterface;
+  private config!: ConfigInterface;
 
-  constructor(logger: LoggerInterface) {
+  constructor(logger: LoggerInterface, config: ConfigInterface) {
     this.logger = logger;
+    this.config = config;
   }
 
   public async init() {
     this.logger.info('Initializing application...');
+    this.logger.info(`Got value from env $PORT: ${this.config.get('PORT')}`);
+    this.logger.info(`DB_HOST: ${this.config.get('DB_HOST')}`);
   }
 }
--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,13 +1,28 @@
+import { config } from 'dotenv';
+import { LoggerInterface } from '../logger/logger.interface.js';
 import { ConfigInterface } from './config.interface.js';
+import { configSchema, ConfigSchema } from './config.schema.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: NodeJS.ProcessEnv;
+  private config: ConfigSchema;
+  private logger: LoggerInterface;
 
-  constructor() {
-    this.config = process.env;
+  constructor(logger: LoggerInterface) {
+    this.logger = logger;
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Maybe the file doesn\'t exist.');
+    }
+
+    configSchema.load({});
+    configSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configSchema.getProperties();
+    this.logger.info('.env file has found and succesfilly parsed');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof ConfigSchema>(key: T) {
     return this.config[key];
   }
 }
